752eddaa969df9a65f2fa620fcdfcb8748ff4df2
onchirim <64867546+onchirim@users.noreply.github.com>
2021-03-08 10:51:14 -0600

    first commit

--- client/store.js
+++ client/store.js
@@ -1,6 +1,6 @@
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import reducers from "./reducers/index.js";
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from './reducers/index.js';
 
 // we are adding composeWithDevTools here to get easy access to the Redux dev tools
 const store = createStore(reducers, composeWithDevTools());
